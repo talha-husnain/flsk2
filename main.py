cd24cab97b2d40fdb1df7661e672a1efc1cbf427
talha-husnain <92517278+talha-husnain@users.noreply.github.com>
2023-05-22 10:45:11 +0000

    api added

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [{
+  'id': 1,
+  'name': "Talha",
+  'age': 47,
+}, {
+  'id': 2,
+  'name': "Talha",
+  'age': 47,
+}, {
+  'id': 3,
+  'name': "Talha",
+  'age': 47,
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template('home.html', jobs=JOBS, nav="LEARN FLASK")
+
+
+@app.route("/raw")
+def raw():
+  return jsonify(JOBS)
+
 
 if __name__ == "__main__":
-  app.run(host ="0.0.0", debug = True)
+  app.run(host="0.0.0", debug=True)
+ 
